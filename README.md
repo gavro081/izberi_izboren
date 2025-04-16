fc9ca5d7afe3a568196cc585c0d3e246aa88f0a6
Filip Gavrilovski <filipgavrilovski28@gmail.com>
2025-04-16 16:01:38 +0200

    added README

---
+++ README.md
@@ -0,0 +1,22 @@
+# IzberiIzboren
+
+This is a full-stack web application built with:
+
+- **Backend:** Django
+- **Frontend:** React with TypeScript and Vite
+- **Containerized:** Using Docker and Docker Compose
+
+> Make sure you have Docker and Docker Compose installed on your system.
+
+### 1. Clone the repository
+
+```bash
+git clone https://github.com/gavro081/izberi_izboren.git
+cd izberi_izboren
+```
+
+### 2. Build and run the project using Docker Compose
+
+```bash
+docker-compose up --build
+```
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,15 @@
-import { useEffect, useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Home";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Home />,
+	},
+]);
 
 function App() {
-	const [data, setData] = useState("");
-	useEffect(() => {
-		const fetchData = async () => {
-			const response = await fetch("http://localhost:8000/api");
-			const data = await response.json();
-			console.log(data);
-			setData(data.message);
-		};
-		fetchData();
-	}, []);
-
-	return <>{data == "" ? <p>Loading....</p> : <p>{data}</p>}</>;
+	return <RouterProvider router={router} />;
 }
 
 export default App;
