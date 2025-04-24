b17d2154f09fb8d8bcaa81a06de048cb76cbad4f
Andrej Ristikj <60400035+andrejwastaken@users.noreply.github.com>
2025-04-24 20:10:23 +0200

    Added chokidar in docker-compose

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,14 +1,14 @@
 import { Link } from "react-router-dom";
 
 const Home = () => {
-	return (
-		<div className="text-center absolute top-1/2 left-1/2 text-lg">
-			<p>fali home page :( </p>
-			<Link to="/subjects" className="text-blue-700 underline">
-				look at this though
-			</Link>
-		</div>
-	);
+  return (
+    <div className="text-center absolute top-1/2 left-1/2 text-lg">
+      <p>f home page :( </p>
+      <Link to="/subjects" className="text-blue-700 underline">
+        look at this though
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
