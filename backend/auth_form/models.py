4627c020374c42dee5c67e7eba58012c3fe07f5e
Andrej Ristikj <60400035+andrejwastaken@users.noreply.github.com>
2025-04-27 23:58:59 +0200

    Added model for Users

--- backend/auth_form/models.py
+++ backend/auth_form/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+class User(AbstractUser):
+    USER_TYPE_CHOICES = [
+    ('student', 'Student'),
+    ('admin', 'Admin'),
+    ('superadmin', 'Superadmin'),
+    ]
+    user_type = models.CharField(max_length=20, choices=USER_TYPE_CHOICES, default='student')
+    full_name = models.CharField(max_length=255)
+
+    def get_user_type(self):
+        return self.user_type
+
+class Student(models.Model):
+    pass
--- backend/subjects/admin.py
+++ backend/subjects/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Subject, Subject_Info
 # Register your models here.
+
+admin.site.register(Subject)
+admin.site.register(Subject_Info)
