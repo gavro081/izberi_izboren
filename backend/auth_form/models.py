4bc55a50ed2c05a97163ab7df7ebcbf0d26b3c92
Andrej Ristikj <60400035+andrejwastaken@users.noreply.github.com>
2025-04-28 11:02:04 +0200

    Almost finished students model

--- backend/auth_form/models.py
+++ backend/auth_form/models.py
@@ -14,4 +14,38 @@ class User(AbstractUser):
         return self.user_type
 
 class Student(models.Model):
-    pass
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='student_profile')
+    index = models.CharField(max_length=20, unique=True)
+
+    STUDY_TRACK_CHOICES = [
+        ('SIIS', 'SIIS'),
+        ('SEIS', 'SEIS'),
+        ('KI', 'KI'),
+        ('KN', 'KN'),
+        ('IMB', 'IMB'),
+        ('PIT', 'PIT'),
+        ('SSP', 'SSP')
+    ]
+    study_track = models.CharField(max_length=20, choices=STUDY_TRACK_CHOICES)
+
+    current_year = models.PositiveIntegerField()
+    study_effort = models.PositiveIntegerField(help_text="Hours per week")
+
+    preferred_domains = models.JSONField(default=list)
+    preferred_technologies = models.JSONField(default=list)
+    preferred_evaluation = models.JSONField(default=list)
+    favorite_professors = models.JSONField(default=list)
+
+    # finish the subjects relation logic
+    passed_subjects = models.ManyToManyField('subjects.subject', related_name="passed_subjects")
+
+    def update_info(self, new_preferences):
+        self.preferred_domains = new_preferences
+        self.save()
+
+    def get_recommendations(self):
+        # Placeholder for recommendation logic
+        pass
+
+    def get_user_info(self):
+        return self
