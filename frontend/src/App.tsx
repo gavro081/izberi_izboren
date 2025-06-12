4978c89ce8b5ac48ab5f807daf3f5573bc41c4e6
Filip Gavrilovski <filipgavrilovski28@gmail.com>
2025-06-12 16:32:59 +0200

    bug fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import PrivateRoute from "./components/PrivateRoute";
@@ -11,67 +13,65 @@ import Login from "./pages/Login";
 import Recommendations from "./pages/Recommendations";
 import Register from "./pages/Register";
 import SubjectView from "./pages/SubjectView";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
 const Layout = () => (
-  <div className="flex flex-col min-h-screen">
-    <Navbar />
-    <ToastContainer
-      className="custom-toast-container"
-      position="top-right"
-      autoClose={2000}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-      theme="light"
-    />
-    <main className="flex-grow">
-      <Outlet />
-    </main>
-    <Footer />
-  </div>
+	<div className="flex flex-col min-h-screen">
+		<Navbar />
+		<ToastContainer
+			className="custom-toast-container"
+			position="top-right"
+			autoClose={2000}
+			hideProgressBar={false}
+			newestOnTop={false}
+			closeOnClick
+			rtl={false}
+			pauseOnFocusLoss
+			draggable
+			pauseOnHover
+			theme="light"
+		/>
+		<main className="flex-grow">
+			<Outlet />
+		</main>
+		<Footer />
+	</div>
 );
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      { path: "", element: <Home /> },
-      { path: "subjects", element: <CourseCatalog /> },
-      { path: "login", element: <Login /> },
-      { path: "register", element: <Register /> },
-      {
-        path: "recommendations",
-        element: (
-          <PrivateRoute>
-            <Recommendations />
-          </PrivateRoute>
-        ),
-      },
-      {
-        path: "account",
-        element: (
-          <PrivateRoute>
-            <Account />
-          </PrivateRoute>
-        ),
-      },
-      {
-        path: "subjects/:id",
-        element: <SubjectView />,
-      },
-    ],
-  },
+	{
+		path: "/",
+		element: <Layout />,
+		children: [
+			{ path: "", element: <Home /> },
+			{ path: "subjects", element: <CourseCatalog /> },
+			{ path: "login", element: <Login /> },
+			{ path: "register", element: <Register /> },
+			{
+				path: "recommendations",
+				element: (
+					<PrivateRoute>
+						<Recommendations />
+					</PrivateRoute>
+				),
+			},
+			{
+				path: "account",
+				element: (
+					<PrivateRoute>
+						<Account />
+					</PrivateRoute>
+				),
+			},
+			{
+				path: "subjects/:code",
+				element: <SubjectView />,
+			},
+		],
+	},
 ]);
 
 function App() {
-  useAxiosAuth();
-  return <RouterProvider router={router} />;
+	useAxiosAuth();
+	return <RouterProvider router={router} />;
 }
 
 export default App;
